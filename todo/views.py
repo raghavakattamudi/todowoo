f7f003f760136bc88aaa17b36650b41e6c2daed5
Raghava Kattamudi <harikattamudi@gmail.com>
2022-04-13 10:43:26 -0500

    d

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,23 @@
+import re
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 # Create your views here.
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+    else:
+#Create a new user
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+                user.save()
+                login(request, user)
+            except IntegrityError:
+                return render(request,'todo/signupuser.html',{'form':UserCreationForm(),'error':'that username has already been taken. Please choose a new user name'})
+
+        else:
+            return render(request,'todo/signupuser.html',{'form':UserCreationForm(),'error':'password did not match'})
